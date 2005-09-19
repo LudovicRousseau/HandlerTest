ff6b127f2ed4c877925cd1b5a3f92d2e556cda02
Ludovic Rousseau <ludovic.rousseau@free.fr>
2005-09-19 14:00:49 +0000

    add support of VERIFY PIN
    git-svn-id: svn://anonscm.debian.org/svn/pcsclite/trunk/HandlerTest@1667 0ce88b0d-b2fd-0310-8134-9614164e65ea

--- JavaCard/src/org/debian/alioth/pcsclite/readertest/readertest.java
+++ JavaCard/src/org/debian/alioth/pcsclite/readertest/readertest.java
@@ -26,7 +26,6 @@ import javacard.framework.*;
 public class readertest extends javacard.framework.Applet
 {
     private final static byte CLA_TEST_READER  = (byte)0x80;
-    private final static byte INS_CASE_1_ODD   = (byte)0x20;
     private final static byte INS_CASE_2_ODD   = (byte)0x21;
     private final static byte INS_CASE_3_ODD   = (byte)0x22;
     private final static byte INS_CASE_4_ODD   = (byte)0x23;
@@ -38,6 +37,7 @@ public class readertest extends javacard.framework.Applet
     private final static byte INS_CASE_2_UNBOUND = (byte)0x3A;
     private final static byte INS_CASE_3_UNBOUND = (byte)0x3C;
     private final static byte INS_CASE_4_UNBOUND = (byte)0x3E;
+    private final static byte INS_VERIFY_PIN = (byte)0x20;
 
     private final static byte pcValueTable[]  = {
     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
@@ -124,7 +124,6 @@ public class readertest extends javacard.framework.Applet
         switch ( apduBuffer[ISO7816.OFFSET_INS] )
         {
             case INS_CASE_1:
-            case INS_CASE_1_ODD:
               if ( (apduBuffer[ISO7816.OFFSET_LC] & (byte) 0x00FF) != 0 )
               {
                 ISOException.throwIt( ISO7816.SW_WRONG_LENGTH );
@@ -286,6 +285,34 @@ public class readertest extends javacard.framework.Applet
               requiredLe = Util.getShort(apduBuffer,ISO7816.OFFSET_P1);
               apdu.setOutgoingLength (requiredLe);
               apdu.sendBytesLong(pcValueTable, (short) 0x0000, requiredLe);
+            break;
+
+            case INS_VERIFY_PIN:
+              // Incoming Data length
+              bytesLeft = (short) (apduBuffer[ISO7816.OFFSET_LC]
+                                               & 0x00FF);
+              if ( bytesLeft == 0 )
+              {
+                ISOException.throwIt( ISO7816.SW_WRONG_LENGTH );
+              }
+              // Get the Data
+              index=0;
+              readCount = apdu.setIncomingAndReceive();
+              while ( bytesLeft > 0 )
+              {
+                for (short i=0; i<readCount; i++)
+                {
+                  if ( ((short)(apduBuffer[(short)(ISO7816.OFFSET_CDATA+i)] & 0x00FF))
+                         != (short)(i + 0x31))
+                  {
+                    short SW = (short) (0x6A00 + i);
+                    ISOException.throwIt( SW );
+                  }
+                  index++;
+                }
+                bytesLeft -= readCount;
+                readCount = apdu.receiveBytes (ISO7816.OFFSET_CDATA);
+              }
             break;
 
             default:
