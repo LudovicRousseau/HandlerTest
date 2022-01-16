bb4436f511de6090d09f0e8cf1ea2684c5463955
Ludovic Rousseau <ludovic.rousseau@free.fr>
2022-01-16 10:59:47 +0100

    debug: display the delta time with the previous log line
    This is used to know the time between 2 log lines.

--- Host/debug.c
+++ Host/debug.c
@@ -12,12 +12,43 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
+
+static void log_time(void)
+{
+	static struct timeval last_time = { 0, 0 };
+	struct timeval new_time = { 0, 0 };
+	struct timeval tmp;
+	int delta;
+
+	gettimeofday(&new_time, NULL);
+	if (0 == last_time.tv_sec)
+		last_time = new_time;
+
+	tmp.tv_sec = new_time.tv_sec - last_time.tv_sec;
+	tmp.tv_usec = new_time.tv_usec - last_time.tv_usec;
+	if (tmp.tv_usec < 0)
+	{
+		tmp.tv_sec--;
+		tmp.tv_usec += 1000000;
+	}
+	if (tmp.tv_sec < 100)
+		delta = tmp.tv_sec * 1000000 + tmp.tv_usec;
+	else
+		delta = 99999999;
+
+	last_time = new_time;
+
+	printf("%.8d ", delta);
+}
 
 void log_msg(const int priority, const char *fmt, ...)
 {
 	va_list argptr;
 
 	(void)priority;
+
+	log_time();
 
 	va_start(argptr, fmt);
 	printf("\33[35m"); /* Magenta */
@@ -35,6 +66,8 @@ void log_xxd(const int priority, const char *msg, const unsigned char *buffer,
 	int i;
 
 	(void)priority;
+
+	log_time();
 
 	printf("%s", msg);
 
